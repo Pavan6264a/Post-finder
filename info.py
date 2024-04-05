d8011c3be72c73f1cc65270ce002c33f2456642e
Pavan6264a <127077968+Pavan6264a@users.noreply.github.com>
2024-04-05 11:49:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  =
-ADMIN        = 1255023013
-CHANNEL      = "NobiDeveloper"
+API_ID       = " "
+API_HASH     = " "
+BOT_TOKEN    = " "
+SESSION      = " "
+DATABASE_URI = " "
+LOG_CHANNEL  = " "
+ADMIN        = " "
+CHANNEL      = " "
